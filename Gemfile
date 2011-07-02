f2093ef7efc2013ba704d393f7c8c0ddbdc794e7
Eric Oestrich <eric@oestrich.org>
2011-07-01 22:26:05 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.rc4'
+
+gem 'pg'
+
+# Asset template engines
+gem 'sass-rails', "~> 3.1.0.rc"
+gem 'coffee-script'
+gem 'uglifier'
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'cucumber-rails'
+  gem 'rspec-rails'
+  gem 'ruby-debug19', :require => 'ruby-debug'
+end
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = 'e1c7acfe6ee418288d5657f7825b557ac50378787d4b7e93d7df35c27878c373f5edb488601208b89a901b21ef58e62f5ab87a225fcf1272f3ac53526e25a40f'
